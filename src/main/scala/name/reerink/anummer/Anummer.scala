0ebd0681d7dccf8b2cc3f34101b5bb62784e70c3
Frank Reerink <nospam@reerink.name>
2011-11-08 22:38:06 +0100

    Speed up of algorithm by factor 4

--- src/main/scala/name/reerink/anummer/Anummer.scala
+++ src/main/scala/name/reerink/anummer/Anummer.scala
@@ -1,7 +1,12 @@
 package name.reerink.anummer;
 
-class Anummer(value: Long) {
+class Anummer(v: Long) {
+	def value = v
+
+	var incr: Long = 1
+
 	def isValid: Boolean = {
+		incr = 1
 		// strValue bevat a0 ... a9
 		val strValue = value.toString()
 		/* 
@@ -11,14 +16,20 @@ class Anummer(value: Long) {
 		if (strValue.length != 10) {
 			return false
 		}
-		// opeenvolgende cijfers zijn niet gelijk aan elkaar
+		// opeenvolgende cijfers mogen niet gelijk aan elkaar zijn
 		var a: Int = strValue(0) - '0'
-		var sum:Long = a
+		var sum: Long = a
 		var multiplier = 1
-		var sum2:Long = a
+		var sum2: Long = a
 		for (i <- Range(1, 10)) {
 			val b: Int = strValue(i) - '0'
 			if (a == b) {
+				if (a != 9 && b != 9) {
+					// Bouw de volgende waarde door het eerste stuk te pakken van het origineel en de rest op 0 te zetten
+					val nextStrValue = strValue.substring(0, i) + (b + 1) + "0000000000".substring(i + 1, 10)
+					incr = nextStrValue.toLong - value
+				}
+				// println("nextValue = " + nextStrValue + ", incr = " + incr)
 				return false
 			}
 			sum += b
--- src/main/scala/name/reerink/anummer/Compute.scala
+++ src/main/scala/name/reerink/anummer/Compute.scala
@@ -25,54 +25,32 @@ object Compute {
 
 	def main(args: Array[String]): Unit = {
 		println("Compute A-nummers")
-		//timer(5, 1, (i) => println("Ready for takeoff: " + i + ", " + new Complex(i, 5 - i).toString()))
 
-		//val env: Env = { case "x" => 3 }
-		//val env2: Env = { case "x" => 6 }
-		//val exp: Tree = Sum(Var("x"), Const(5))
+		val anummers: List[Long] = List(100000000, 1000000000, 1010101024, 1010101025, 1010101026,
+			1010000000, 1010100021, 1010101010, 1010101098, 1010101099, 1010101100, 1010991234)
+		for (anummer <- anummers) {
+			val a = new Anummer(anummer)
+			println("Anummer (" + a + ") valid?=" + a.isValid + ", next = " + a.incr + " -> " + (anummer + a.incr))
+		}
 
-		//println("exp " + exp + " for env: " + env2 + " = " + eval(exp, env2))
-		println("range(1,10)=" + Range(1, 10))
-
-		val a1 = new Anummer(100000000)
-		println("Anummer (" + a1 + ") valid?=" + a1.isValid)
-		val a2 = new Anummer(1000000000)
-		println("Anummer (" + a2 + ") valid?=" + a2.isValid)
-		val a3 = new Anummer(1010101024)
-		println("Anummer (" + a3 + ") valid?=" + a3.isValid)
-		val a4 = new Anummer(1010101025)
-		println("Anummer (" + a4 + ") valid?=" + a4.isValid)
-		val a5 = new Anummer(1010101026)
-		println("Anummer (" + a5 + ") valid?=" + a5.isValid)
+		//return
 
 		val writer: PrintWriter = new PrintWriter(new BufferedWriter(new FileWriter("anummers.txt")));
 
 		var found = 0
 		var a: Long = 1000000000
-		while (a <= 9999999999L) {
+		while (a < 9999999999L) {
 			val anr = new Anummer(a)
 			if (anr.isValid) {
-				if (found % 1000 == 0) {
+				if (found % 1000 == 0 ) {
 					println("Found (" + found + ") A-nummers, current: " + anr)
 				}
-				writer.println(anr.toString())
 				found += 1
-			}
-			a += 1
+				writer.println(anr.toString())
+			} 
+			a += anr.incr
 		}
 		writer.close()
 	}
 
-	abstract class Tree
-	case class Sum(l: Tree, r: Tree) extends Tree
-	case class Var(v: String) extends Tree
-	case class Const(c: Int) extends Tree
-
-	type Env = String => Int
-
-	def eval(t: Tree, e: Env): Int = t match {
-		case Sum(l, r) => eval(l, e) + eval(r, e)
-		case Var(x) => e(x)
-		case Const(c) => c
-	}
 }
